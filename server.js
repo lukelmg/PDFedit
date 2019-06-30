9afd7726a72b4b336431801e6935fb569a9b28b3
Glitch (hello-express) <none>
2019-06-30 21:28:12 +0000

    🗺🌪 Checkpoint ./server.js:645589/123 ./public/images/placeholder.txt:645589/75

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const cryptoRandomString = require('crypto-random-string');
 const app = express();
 
 app.use(express.static('public'));
 app.use(fileUpload());
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.post('/upload', function(req, res) {
+app.post('/upload', (req, res) => {
   if (Object.keys(req.files).length == 0) {
     return res.status(400).send('No files were uploaded.');
   }
@@ -18,7 +19,7 @@ app.post('/upload', function(req, res) {
   let sampleFile = req.files.image;
 
   // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv('/public/images/filename.jpg', function(err) {
+  sampleFile.mv('public/upload/filename.jpg', (err) => {
     if (err)
       return res.status(500).send(err);
 
@@ -26,6 +27,6 @@ app.post('/upload', function(req, res) {
   });
 });
 
-const listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
