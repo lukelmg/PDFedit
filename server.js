2479c06bebcc4b5d328be7ca56e046a00c2ef706
Glitch (hello-express) <none>
2019-06-30 23:07:10 +0000

    👣🐝 Checkpoint ./server.js:645589/3568

--- server.js
+++ server.js
@@ -1,15 +1,43 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const multer = require('multer');
+const fileType = require('file-type');
+
 const app = express();
+const accepted_extensions = ['jpg', 'png', 'gif'];
 
 app.use(express.static('public'));
 app.use(fileUpload());
+
+const upload = multer({
+    limits: { 
+        fileSize: 5 * 1024 * 1024,  // 5 MB upload limit
+        files: 1                    // 1 file
+    },
+    fileFilter: (req, file, cb) => {
+        // if the file extension is in our accepted list
+        if (accepted_extensions.some(ext => file.originalname.endsWith("." + ext))) {
+            return cb(null, true);
+        }
+
+        // otherwise, return error
+        return cb(new Error('Only ' + accepted_extensions.join(", ") + ' files are allowed!'));
+    }
+});
+
+// Middleware for validating file format
+function validate_format(req, res, next) {
+    let mime = fileType(req.files.data);
+    if(!mime || !accepted_extensions.includes(mime.ext))
+        return next(new Error('The uploaded file is not in ' + accepted_extensions.join(", ") + ' format!'));
+    next();
+}
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.post('/upload', (req, res) => {
+app.post('/upload', upload.single('image'), validate_format, (req, res, next) => {
   if (Object.keys(req.files).length == 0) {
     return res.status(400).send('No files were uploaded.');
   }
