b8a5fcb740c30cdb8825eb2826b67b18075db1c7
Glitch (hello-express) <none>
2019-06-30 23:18:09 +0000

    🎿😹 Checkpoint ./server.js:645589/2391

--- server.js
+++ server.js
@@ -10,41 +10,43 @@ app.use(express.static('public'));
 app.use(fileUpload());
 
 const upload = multer({
-    limits: { 
-        fileSize: 5 * 1024 * 1024,  // 5 MB upload limit
-        files: 1                    // 1 file
-    },
-    fileFilter: (req, file, cb) => {
-        // if the file extension is in our accepted list
-        if (accepted_extensions.some(ext => file.originalname.endsWith("." + ext))) {
-            return cb(null, true);
-        }
-
-        // otherwise, return error
-        return cb(new Error('Only ' + accepted_extensions.join(", ") + ' files are allowed!'));
+  limits: { 
+    fileSize: 5 * 1024 * 1024,  // 5 MB upload limit
+    files: 1                    // 1 file
+  },
+  fileFilter: (req, file, cb) => {
+    // if the file extension is in our accepted list
+    if (accepted_extensions.some(ext => file.originalname.endsWith("." + ext))) {
+      return cb(null, true);
     }
+
+    // otherwise, return error
+    return cb(new Error('Only ' + accepted_extensions.join(", ") + ' files are allowed!'));
+  }
 });
 
 // Middleware for validating file format
-function validate_format(req, res, next) {
-    let mime = fileType(req.files.data);
-    if(!mime || !accepted_extensions.includes(mime.ext))
-        return next(new Error('The uploaded file is not in ' + accepted_extensions.join(", ") + ' format!'));
-    next();
+function validate_format(req, res, next) { 
+  // If no files were selected
+  if (!req.files) {
+      return res.status(400).send('No files were uploaded.');
+  };
+  
+  // Image/mime validation
+  let mime = fileType(req.files.image.data);
+  if(!mime || !accepted_extensions.includes(mime.ext))
+    return next(res.status(500).send('The uploaded file is not in ' + accepted_extensions.join(", ") + ' format!'));
+  next();
 }
 
+// Index get route
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// Upload post route
 app.post('/upload', upload.single('image'), validate_format, (req, res, next) => {
-  if (Object.keys(req.files).length == 0) {
-    return res.status(400).send('No files were uploaded.');
-  }
-
   let upFile = req.files.image;
-  console.log(req.files.image.md5);
-  
   let id = req.files.image.md5
   
   // Use the mv() method to place the file somewhere on your server
@@ -56,6 +58,7 @@ app.post('/upload', upload.single('image'), validate_format, (req, res, next) =>
   });
 });
 
+// Server listener
 const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
