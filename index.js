76df18203b427833c3a683bf6f4494d1e37ae020
lukelmg <lukegutman14@gmail.com>
2022-07-01 15:51:53 -0400

    working!

--- index.js
+++ index.js
@@ -2,29 +2,40 @@ var hummus = require('hummus');
 var _ = require('lodash');
 var extractText = require('./lib/text-extraction');
 
+var run  = 0;
+var e = 0;
+var t = 0;
+
 function runMe() {
-    var fileToRun = './samples/HighLevelContentContext.pdf';
+    var fileToRun = './samples/pogbatch.pdf';
     var pdfReader = hummus.createReader(fileToRun);
 
     var pagesPlacements = extractText(pdfReader);
-    
-    //console.log('pages text placements',JSON.stringify(pagesPlacements,null,2));
 
     var pdfWriter = hummus.createWriterToModify(fileToRun,{modifiedFilePath:'./samples/test_out.pdf'});
     for(var i=0;i<pagesPlacements.length;++i) {
         var pageModifier = new hummus.PDFPageModifier(pdfWriter,i);
 		var cxt = pageModifier.startContext().getContext();
         pagesPlacements[i].forEach((placement)=> {
-            if (placement.text == 'Circles') {
+            if (placement.text == 'i' && pagesPlacements[i][e+1].text == 'n' && pagesPlacements[i][e+2].text == 'k') {
                 cxt.q();
-                cxt.cm.apply(cxt,placement.matrix);
-                cxt.drawRectangle(placement.localBBox[0],placement.localBBox[1],placement.localBBox[2]-placement.localBBox[0],placement.localBBox[3]-placement.localBBox[1],{color:'Red',width:2});
+                cxt.drawRectangle(placement.matrix[4]-2, 782-placement.matrix[5], 30, 12,{color:'Red',width:2})
                 cxt.Q();
+                run++;
             }
+            if (pagesPlacements[i][e].text == 't' && pagesPlacements[i][e+1].text == 'o' && pagesPlacements[i][e+2].text == 'n' && pagesPlacements[i][e+3].text == 'e' && pagesPlacements[i][e+4].text == 'r') {
+                cxt.q();
+                cxt.drawRectangle(placement.matrix[4]-2, 782-placement.matrix[5], 30, 12,{color:'Green',width:2})
+                cxt.Q();
+                run++;
+            }
+            e++;
         });
+        e = 0;
 		pageModifier.endContext().writePage();
     }
     pdfWriter.end();
 }
+
 
 runMe();
